d3b32e0a515195f6d79e43381c7bde3c68783eea
rhuan <rhuannixon@gmail.com>
2024-03-17 20:13:04 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fiap-tech-challenge-lambda
+
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+var (
+	timeout = time.Millisecond * 5
+)
+
+func handler(event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Println("authenticating user")
+	randNum := rand.Int63()
+
+	mapRes := map[string]int64{"random_number": randNum}
+	body, err := json.Marshal(mapRes)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       "coundnt marshal the body",
+		}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers: map[string]string{
+			"Content-Type":  "application/json",
+			"Authorization": "test-123",
+		},
+		Body: string(body),
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
